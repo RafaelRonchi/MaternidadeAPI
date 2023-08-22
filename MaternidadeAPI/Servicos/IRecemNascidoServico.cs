7f4d3b5865829f0bf0d3548714f7d105e5e23be1
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-22 16:32:57 -0300

    Auto stash before merge of "master" and "origin/master"

--- MaternidadeAPI/Servicos/IRecemNascidoServico.cs
+++ MaternidadeAPI/Servicos/IRecemNascidoServico.cs
@@ -1,6 +1,18 @@
-﻿namespace MaternidadeAPI.Servicos
+﻿using MaternidadeAPI.Modelo;
+
+namespace MaternidadeAPI.Servicos
 {
     public interface IRecemNascidoServico
     {
+        Task<List<RecemNascidoModelo>> GetRecemNascidosMae(int id);
+
+        Task<List<RecemNascidoModelo>> GetRecemNascidosMaeParto(int id, string parto);
+
+        Task CreateRecemNascido(RecemNascidoModelo recemNascido);
+        Task<RecemNascidoModelo> UpdateRecemNascido(RecemNascidoModelo recemNascido);
+        Task DeleterRecemNascido(int Id);
+        Task<RecemNascidoModelo> GetRecemNascidoGenero(string Genero);
+        Task<RecemNascidoModelo> GetRecemNascidoId(int Id);
+        Task<RecemNascidoModelo> GetRecemNascidoPeso(int Peso);
     }
 }
--- MaternidadeAPI/Servicos/RecemNascidoServico.cs
+++ MaternidadeAPI/Servicos/RecemNascidoServico.cs
@@ -1,18 +1,60 @@
-﻿using MaternidadeAPI.Data;
+using MaternidadeAPI.Data;
+using MaternidadeAPI.Modelo;
+using Microsoft.EntityFrameworkCore;
 
 namespace MaternidadeAPI.Servicos
 {
     public class RecemNascidoServico : IRecemNascidoServico
     {
-        private readonly DataContext _dataContext;
-
-        public RecemNascidoServico(DataContext dataContext)
+        private readonly DataContext _contexto;
+        public RecemNascidoServico(DataContext context) { _contexto = context; }
+        public async Task CreateRecemNascido(RecemNascidoModelo recemNascido)
         {
-            _dataContext = dataContext;
+            _contexto.RecemNascidos.Add(recemNascido);
+            await _contexto.SaveChangesAsync();
         }
 
+        public async Task DeleteRecemNascido(int Id)
+        {
+            var recemNascido = await _contexto.RecemNascidos.FindAsync(Id);
+            if (recemNascido != null)
+            {
+                _contexto.RecemNascidos.Remove(recemNascido);
+                await _contexto.SaveChangesAsync();
+            }
+        }
 
+        public async Task<RecemNascidoModelo> GetRecemNascidoGenero(string Genero)
+        {
+            return await _contexto.RecemNascidos.FirstOrDefaultAsync(r => r.Genero == Genero);
+        }
 
+        public async Task<RecemNascidoModelo> GetRecemNascidoId(int Id)
+        {
+            return await _contexto.RecemNascidos.FindAsync(Id);
+        }
+
+        public async Task<RecemNascidoModelo> GetRecemNascidoPeso(int Peso)
+        {
+            return await _contexto.RecemNascidos.FirstOrDefaultAsync(r => r.PesoGramas == Peso);
+        }
+
+        public async Task<List<RecemNascidoModelo>> GetRecemNascidosMae(int id)
+        {
+            return await _contexto.RecemNascidos.Where(r => r.MaeId == id).ToListAsync();
+        }
+
+        public async Task<List<RecemNascidoModelo>> GetRecemNascidosMaeParto(int id, string parto)
+        {
+            return await _contexto.RecemNascidos.Where(r => r.MaeId == id && r.TipoParto == parto).ToListAsync();
+        }
+
+        public async Task<RecemNascidoModelo> UpdateRecemNascido(RecemNascidoModelo recemNascido)
+        {
+            _contexto.Entry(recemNascido).State = EntityState.Modified;
+            await _contexto.SaveChangesAsync();
+            return recemNascido;
+        }
 
     }
 }

cf371fe44af773b8ce11af95a355b4d27e4e7c3c
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-22 14:58:02 -0300

    Data context

---
+++ MaternidadeAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using MaternidadeAPI.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaternidadeAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<MaeModelo> Maes { get; set; }
+        public DbSet<RecemNascidoModelo> RecemNascido { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

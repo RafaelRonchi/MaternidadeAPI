f0c4dd47af441d78284afb9ba6066993af1ae7c2
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-22 15:28:54 -0300

    Adicionando migrations

--- MaternidadeAPI/Data/DataContext.cs
+++ MaternidadeAPI/Data/DataContext.cs
@@ -13,6 +13,11 @@ namespace MaternidadeAPI.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<RecemNascidoModelo>()
+                .HasOne(m => m.Mae)
+                .WithMany()
+                .HasForeignKey(m => m.MaeId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
---
+++ MaternidadeAPI/Migrations/20230822182524_InitialMigration.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaternidadeAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Maes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Rg = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EstadoCivil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Profissao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Etnia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Historico = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecemNascido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Genero = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PesoGramas = table.Column<int>(type: "int", nullable: false),
+                    AlturaCentimetros = table.Column<int>(type: "int", nullable: false),
+                    TipoParto = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Apgar = table.Column<int>(type: "int", nullable: false),
+                    CondicaoSaude = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    MaeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecemNascido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecemNascido_Maes_MaeId",
+                        column: x => x.MaeId,
+                        principalTable: "Maes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecemNascido_MaeId",
+                table: "RecemNascido",
+                column: "MaeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecemNascido");
+
+            migrationBuilder.DropTable(
+                name: "Maes");
+        }
+    }
+}

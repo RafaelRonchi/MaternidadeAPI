ef60885909865331f0ee19822fc89bbd251514c1
paulolisboa38 <paulolisboa38@gmail.com>
2023-08-22 15:15:08 -0300

    Atualizacao MaeModelo

--- MaternidadeAPI/Modelo/MaeModelo.cs
+++ MaternidadeAPI/Modelo/MaeModelo.cs
@@ -5,19 +5,28 @@ namespace MaternidadeAPI.Modelo
     public class MaeModelo
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; } = string.Empty;
+
         public string Sobrenome { get; set; } = string.Empty;
 
-        [RegularExpression(@"^\d{1,3}:\d{2}$", ErrorMessage = "A duração deve estar no formato mm:ss ou hhh:mm:ss.")]
-        public string DataNascimento { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; } = DateTime.MinValue;
+
         public string Rg { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(11)]
         public string Cpf { get; set; } = string.Empty;
+
         public string Endereco { get; set; } = string.Empty;
+
+        [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; } = string.Empty;
         public string EstadoCivil { get; set; } = string.Empty;
         public string Profissao { get; set; } = string.Empty;
-
-        public string Etinia { get; set;} = string.Empty;
+        public string Etnia { get; set; } = string.Empty;
         public string Historico { get; set; } = string.Empty;
 
         // Relação
--- MaternidadeAPI/Modelo/RecemNascidoModelo.cs
+++ MaternidadeAPI/Modelo/RecemNascidoModelo.cs
@@ -1,6 +1,34 @@
-﻿namespace MaternidadeAPI.Modelo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaternidadeAPI.Modelo
 {
     public class RecemNascidoModelo
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Genero { get; set; } = string.Empty;
+
+        public DateTime DataNascimento { get; set; } = DateTime.MinValue;
+        public int PesoGramas { get; set; } = 0;
+        public int AlturaCentimetros { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string TipoParto { get; set; }
+
+        public int Apgar { get; set; }
+
+        [MaxLength(500)]
+        public string CondicaoSaude { get; set; }
+
+        public int MaeId { get; set; }
+
+        public MaeModelo Mae { get; set; }
     }
 }

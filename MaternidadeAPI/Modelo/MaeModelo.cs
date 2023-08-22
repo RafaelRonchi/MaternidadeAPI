2c903291a99262427a0511592a9c4f43dcdfbf7e
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-22 15:07:12 -0300

    Adicionando o string.Empty;

--- MaternidadeAPI/Modelo/MaeModelo.cs
+++ MaternidadeAPI/Modelo/MaeModelo.cs
@@ -5,20 +5,20 @@ namespace MaternidadeAPI.Modelo
     public class MaeModelo
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
 
         [RegularExpression(@"^\d{1,3}:\d{2}$", ErrorMessage = "A duração deve estar no formato mm:ss ou hhh:mm:ss.")]
         public string DataNascimento { get; set; } = string.Empty;
-        public string Rg { get; set; }
-        public string Cpf {  get; set; }
-        public string Endereco { get; set; }
-        public string Telefone { get; set; }
-        public string EstadoCivil { get; set; }
-        public string Profissao { get; set; }
+        public string Rg { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Endereco { get; set; } = string.Empty;
+        public string Telefone { get; set; } = string.Empty;
+        public string EstadoCivil { get; set; } = string.Empty;
+        public string Profissao { get; set; } = string.Empty;
 
-        public string Etinia { get; set;}
-        public string Historico { get; set; }
+        public string Etinia { get; set;} = string.Empty;
+        public string Historico { get; set; } = string.Empty;
 
         // Relação
     }
